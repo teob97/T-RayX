c15fa3eee62dcef5b49578f03477e82042ea46eb
teobaratto <teobaratto@client39778.mobile.eduroam.unimi.it>
2022-03-09 16:00:09 +0100

    bug fixed, test ok

--- src/tools.nim
+++ src/tools.nim
@@ -36,16 +36,6 @@ func newHdrImage*(width, height : int) : HdrImage =
 func pixel_offset*(img : HdrImage; x, y : int) : int =
     return y * img.width + x
 
-# get_pixel
-func get_pixel(img : HdrImage; x, y : int) : Color =
-    assert valid_coordinates(img, x, y)
-    return img.pixels[y * img.width + x]
-
-# set_pixel
-func set_pixel(img : HdrImage; x, y : int; new_col : Color) =
-    assert valid_coordinates(img, x, y)
-    img.pixels[y * img.width + x] = new_col
-
 #OPERATOR OVERLOAD
 
 func `+` *(c1 : Color, c2 : Color): Color =
@@ -66,3 +56,49 @@ func `*` *(c: Color, a: float32) : Color =
 func are_close *(c1, c2: Color; epsilon : float32 = 1e-5) : bool =
     var dif : Color = c1 - c2
     return dif.r < epsilon and dif.g < epsilon and dif.b < epsilon
+
+#FUNCTIONS TEST
+
+func valid_coordinates*(img : HdrImage; width, height : int) : bool =
+    return width >= 0 and width < img.width and height >= 0 and height < img.height 
+
+func test_image_creation*() =
+    var img = newHdrImage(7, 4)
+    assert img.width == 7
+    assert img.height == 4
+
+func test_coordinates*() =
+    var img = newHdrImage(7, 4)
+    assert valid_coordinates(img, 0, 0)
+    assert valid_coordinates(img, 6, 3)
+    assert not valid_coordinates(img, -1, 0)
+    assert not valid_coordinates(img, 0, -1)
+    assert not valid_coordinates(img, 7, 0)
+    assert not valid_coordinates(img, 0, 4)
+
+func test_pixel_offset*() =
+    var img = newHdrImage(7, 4)
+
+    assert pixel_offset(img, 0, 0) == 0
+    assert pixel_offset(img, 3, 2) == 17
+    assert pixel_offset(img, 6, 3) == 7 * 4 - 1
+
+
+# get_pixel
+func get_pixel*(img : HdrImage; x, y : int) : Color =
+    assert valid_coordinates(img, x, y)
+    return img.pixels[y * img.width + x]
+
+# set_pixel
+func set_pixel*(img : var HdrImage; x, y : int; new_col : Color) =
+    assert valid_coordinates(img, x, y)
+    img.pixels[pixel_offset(img, x, y)].r = new_col.r
+    img.pixels[pixel_offset(img, x, y)].g = new_col.g
+    img.pixels[pixel_offset(img, x, y)].b = new_col.b
+
+func test_get_set_pixel*() =
+    var 
+        img = newHdrImage(7, 4)
+        reference_color = newColor(1.0, 2.0, 3.0)
+    set_pixel(img, 3, 2, reference_color)
+    assert are_close(reference_color, img.get_pixel(3, 2))
--- tests/test_basictypes.nim
+++ tests/test_basictypes.nim
@@ -1,39 +1,5 @@
 import ../src/tools
 
-#FUNCTIONS
-
-func valid_coordinates(img : HdrImage; width, height : int) : bool =
-    return width >= 0 and width < img.width and height >= 0 and height < img.height 
-
-func test_image_creation() =
-    var img = newHdrImage(7, 4)
-    assert img.width == 7
-    assert img.height == 4
-
-func test_coordinates() =
-    var img = newHdrImage(7, 4)
-    assert valid_coordinates(img, 0, 0)
-    assert valid_coordinates(img, 6, 3)
-    assert not valid_coordinates(img, -1, 0)
-    assert not valid_coordinates(img, 0, -1)
-    assert not valid_coordinates(img, 7, 0)
-    assert not valid_coordinates(img, 0, 4)
-
-func test_pixel_offset() =
-    var img = newHdrImage(7, 4)
-
-    assert pixel_offset(img, 0, 0) == 0
-    assert pixel_offset(img, 3, 2) == 17
-    assert pixel_offset(img, 6, 3) == 7 * 4 - 1
-
-
-func test_get_set_pixel() =
-    var img = newHdrImage(7, 4)
-
-    reference_color = Color(1.0, 2.0, 3.0)
-    img.set_pixel(3, 2, reference_color)
-    assert are_colors_close(reference_color, img.get_pixel(3, 2))
-
 #TESTS
 
 when isMainModule:
@@ -46,5 +12,5 @@ when isMainModule:
     
     test_image_creation()
     test_coordinates()
-    #test_get_set_pixel()
+    test_get_set_pixel()
     test_pixel_offset()
