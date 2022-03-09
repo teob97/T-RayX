8c00afbc6474dd39d830977952c2f62460d18559
teobaratto <teobaratto@client39778.mobile.eduroam.unimi.it>
2022-03-09 12:53:38 +0100

    nimble added

--- tests/test_basictypes.nim
+++ tests/test_basictypes.nim
@@ -1,5 +1,5 @@
-import ../src/tools.nim
+import ../src/tools
 
 when isMainModule:
-    assert Color(1.0, 2.0, 3.0) + Color(3.0, 4.0, 5.0) == Color(5.0, 6.0, 8.0)
-    assert Color(6.0, 4.0, 5.0) - Color(1.0, 2.0, 3.0) == Color(7.0, 2.0, 2.0)
+    assert newColor(1.0, 2.0, 3.0) + newColor(3.0, 4.0, 5.0) == newColor(4.0, 6.0, 8.0)
+    assert newColor(6.0, 4.0, 5.0) - newColor(1.0, 2.0, 3.0) == newColor(5.0, 2.0, 2.0)
