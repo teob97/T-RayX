3ae38bb64b9590535b151ef45d11b22ff14dd512
Eleonora Gatti <eleonoragatti@client5745.mobile.eduroam.unimi.it>
2022-03-09 14:38:30 +0100

    HdrImage constructor added

--- src/tools.nim
+++ src/tools.nim
@@ -14,6 +14,14 @@ func newColor*(r : float32 = 0.0; g : float32 = 0.0; b : float32 = 0.0) : Color 
     C.b = b
     return C
 
+func newHdrImage*(width, height : int) : HdrImage =
+    var Hdr : HdrImage
+    var C : Color = newColor(0.0, 0.0, 0.0)
+    Hdr.width = width
+    Hdr.height = height
+    Hdr.pixels = newSeq[Color](width*height)
+    for i in 0..width*height:
+        Hdr.pixels[i] = C
 
 func `+` *(c1 : Color, c2 : Color): Color =
     return newColor(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b)
