96993bdf2967cdd6f753beedd87bf729aca63def
teobaratto <teobaratto@client39778.mobile.eduroam.unimi.it>
2022-03-09 14:39:19 +0100

    tests

--- src/tools.nim
+++ src/tools.nim
@@ -1,3 +1,5 @@
+#CLASS
+
 type
     Color* = object
         r*, g*, b* : float32
@@ -6,6 +8,7 @@ type
         width*, height*: int
         pixels* : seq[Color]
         
+#CONSTRUCTORS
 
 func newColor*(r : float32 = 0.0; g : float32 = 0.0; b : float32 = 0.0) : Color =
     var C : Color
@@ -14,6 +17,7 @@ func newColor*(r : float32 = 0.0; g : float32 = 0.0; b : float32 = 0.0) : Color 
     C.b = b
     return C
 
+#OPERATOR OVERLOAD
 
 func `+` *(c1 : Color, c2 : Color): Color =
     return newColor(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b)
@@ -25,6 +29,9 @@ func `*` *(c1, c2: Color) : Color =
     return newColor(c1.r * c2.r, c1.g * c2.g, c1.b * c2.b)
 
 func `*` *(a: float32, c: Color) : Color =
+    return newColor(c.r * a, c.g * a, c.b * a)
+
+func `*` *(c: Color, a: float32) : Color =
     return newColor(c.r * a, c.g * a, c.b * a)
 
 func are_close *(c1, c2: Color; epsilon : float32 = 1e-5) : bool =
--- tests/test_basictypes.nim
+++ tests/test_basictypes.nim
@@ -1,5 +1,41 @@
 import ../src/tools
 
+#FUNCTIONS
+
+func test_image_creation() =
+    img = HdrImage(7, 4)
+    assert img.width == 7
+    assert img.height == 4
+
+func test_coordinates():
+    img = HdrImage(7, 4)
+    assert img.valid_coordinates(0, 0)
+    assert img.valid_coordinates(6, 3)
+    assert not img.valid_coordinates(-1, 0)
+    assert not img.valid_coordinates(0, -1)
+    assert not img.valid_coordinates(7, 0)
+    assert not img.valid_coordinates(0, 4)
+
+func test_pixel_offset():
+    img = HdrImage(7, 4)
+
+    assert img.pixel_offset(0, 0) == 0
+    assert img.pixel_offset(3, 2) == 17
+    assert img.pixel_offset(6, 3) == 7 * 4 - 1
+
+func test_get_set_pixel():
+    img = HdrImage(7, 4)
+
+    reference_color = Color(1.0, 2.0, 3.0)
+    img.set_pixel(3, 2, reference_color)
+    assert are_colors_close(reference_color, img.get_pixel(3, 2))
+
+#TESTS
+
 when isMainModule:
     assert newColor(1.0, 2.0, 3.0) + newColor(3.0, 4.0, 5.0) == newColor(4.0, 6.0, 8.0)
     assert newColor(6.0, 4.0, 5.0) - newColor(1.0, 2.0, 3.0) == newColor(5.0, 2.0, 2.0)
+    assert newColor(1.0, 2.0, 3.0) * newColor(2.0, 3.0, 4.0) == newColor(2.0, 6.0, 12.0)
+    assert 3.0 * newColor(1.0, 2.0, 3.0) == newColor(3.0, 6.0, 9.0)
+    assert newColor(1.0, 2.0, 3.0) * 3.0 == newColor(3.0, 6.0, 9.0)
+    assert are_close(newColor(1.11113, 2.0, 3.0), newColor(1.11113, 2.0, 3.0)) == true
