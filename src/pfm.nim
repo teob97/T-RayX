6cf382291695c6413cb3e38af4c6f5288ad5bac4
teobaratto <teobaratto@client39778.mobile.eduroam.unimi.it>
2022-03-16 16:58:55 +0100

    Added parseImage

--- src/pfm.nim
+++ src/pfm.nim
@@ -1,5 +1,9 @@
 import std/endians
 import std/streams
+import std/strutils
+
+type
+    InvalidPfmFileFormat = object of CatchableError
 
 proc readFloat*(stream: Stream, endianness: float) : float32 =
     
@@ -11,3 +15,22 @@ proc readFloat*(stream: Stream, endianness: float) : float32 =
     if endianness < 0:
         littleEndian32(addr num, addr x)
     return num
+
+proc parseImgSize*(line: string): (int,int) =
+    var 
+        elements = split(line)
+        width : int
+        height : int
+
+    if len(elements) != 2:
+        raise newException(InvalidPfmFileFormat, "Invalid image size specification")
+
+    try:
+        width = elements[0].parseInt()
+        height = elements[1].parseInt()
+        if width < 0 or height < 0:
+            raise newException(ValueError, "")
+    except ValueError:
+        raise newException(InvalidPfmFileFormat, "Ivalid width/height")
+
+    return (width, height)
