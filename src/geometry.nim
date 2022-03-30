609966dfff2e3318f178a6f80e738bac9b535c78
Eleonora Gatti <eleonoragatti@client5745.mobile.eduroam.unimi.it>
2022-03-30 15:32:56 +0200

    Added functions in geometry.nim

--- src/geometry.nim
+++ src/geometry.nim
@@ -1,3 +1,5 @@
+import std/math
+
 ###############
 #VECTOR OBJECT#
 ###############
@@ -36,3 +38,64 @@ template define_string_conversion(t: typedesc) =
   proc `$`(arg : t): string =
     var buffer : string = $t & "(" & $arg.x & ", " & $arg.y & ", " & $arg.z & ")"
     return buffer
+
+#Operation fname (+ or -) between object of different type
+template define_3dop*(fname: untyped, type1: typedesc, type2: typedesc, rettype: typedesc) =
+  proc fname*(a: type1, b: type2): rettype =
+    result.x = fname(a.x, b.x)
+    result.y = fname(a.y, b.y)
+    result.z = fname(a.z, b.z)
+
+define_3dop(`+`, Vec, Vec, Vec)
+define_3dop(`-`, Vec, Vec, Vec)
+define_3dop(`+`, Vec, Point, Point)
+define_3dop(`+`, Point, Vec, Point)
+define_3dop(`-`, Point, Vec, Point)
+define_3dop(`+`, Normal, Normal, Normal)
+define_3dop(`-`, Normal, Normal, Normal)
+
+#Scalar product between Vector and/or Normal
+template define_dot*(type1: typedesc, type2: typedesc) =
+  proc `*`*(a: type1, b: type2): float =
+    return a.x * b.x +  a.y * b.y +  a.z * b.z
+
+define_dot(Vec, Vec)
+define_dot(Vec, Normal)
+define_dot(Normal, Vec)
+
+#Squared norm of a Vector or a Normal
+template define_squared_norm*(t: typedesc) =
+  proc squared_norm*(a: t): float =
+    return a.x * a.x +  a.y * a.y +  a.z * a.z
+
+define_squared_norm(Vec)
+define_squared_norm(Normal)
+
+#Norm of a Vector or a Normal
+template define_norm*(t: typedesc) =
+  proc norm*(a: t): float =
+    return sqrt(squared_norm(a))
+
+define_norm(Vec)
+define_norm(Normal)
+
+#Modify the vector's norm so that it becomes equal to 1
+template define_normalization*(t: typedesc) =
+  proc normalization*(a: t) =
+    a = a * (1/norm(a))
+
+define_normalization(Vec)
+define_normalization(Normal)
+
+#Conversion from Vec to Normal
+proc VecToNormal*(v: Vec) : Normal =
+  normalization(v)
+  result.x = v.x
+  result.y = v.y
+  result.z = v.z
+
+#Conversion from Point to Vec
+proc PointToVec*(p: Point) : Vec =
+  result.x = p.x
+  result.y = p.y
+  result.z = p.z
