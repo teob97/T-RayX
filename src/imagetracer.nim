a5267a1fca6e3fa6983b270e017414ff8d4072a4
Eleonora Gatti <eleonoragatti@MacBook-di-Eleonora.local>
2022-05-19 11:24:19 +0200

    Added antialiasing

--- src/imagetracer.nim
+++ src/imagetracer.nim
@@ -14,15 +14,18 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>. ]#
     
-import basictypes, cameras, renderer
+import basictypes, cameras, renderer, pcg
+import std/math
 
 type
   ImageTracer* = object
     ## Trace an image by shooting light rays through each of its pixels.
     image* : HdrImage
     camera* : Camera
+    samples_per_side* : int
+    pcg* : PCG
 
-proc newImageTracer*(image : HdrImage, camera : Camera): ImageTracer =
+proc newImageTracer*(image : HdrImage, camera : Camera, samples_per_side : int = 0, pcg : PCG = newPCG()): ImageTracer =
   ## Initialize an ImageTracer object
   ## The parameter `image` must be a `HdrImage` object that has already been initialized.
   ## The parameter `camera` must be a descendeant of the `Camera` object.
@@ -30,6 +33,8 @@ proc newImageTracer*(image : HdrImage, camera : Camera): ImageTracer =
   ## image, using the random number generator `pcg`.
   result.image = image
   result.camera = camera
+  result.samples_per_side = samples_per_side
+  result.pcg = pcg
 
 proc fireRay*(imageT : ImageTracer, col : int, row : int, u_pixel = 0.5, v_pixel = 0.5): Ray =
   ## Shoot one light ray through image pixel (col, row)
@@ -50,6 +55,20 @@ proc fireAllRays*(imageT : var ImageTracer, renderer : Renderer) =
   var color : Color
   for row in 0..<(imageT.image.height):
     for col in 0..<(imageT.image.width):
-      ray = imageT.fire_ray(col, row)
-      color = renderer.render(ray)
-      imageT.image.setPixel(col, row, color)
+      var cum_color = newColor(0.0, 0.0, 0.0)
+      # If we want antialiasing -> samples_per_side should be > 0
+      if imageT.samples_per_side > 0:
+        # Run stratified sampling over the pixel's surface
+        for inter_pixel_row in 0..imageT.samples_per_side:
+          for inter_pixel_col in 0..imageT.samples_per_side:
+            let
+              u_pixel = (inter_pixel_col.float + imageT.pcg.random_float()) / imageT.samples_per_side.float
+              v_pixel = (inter_pixel_row.float + imageT.pcg.random_float()) / imageT.samples_per_side.float
+            ray = imageT.fireRay(col=col, row=row, u_pixel=u_pixel, v_pixel=v_pixel)
+            cum_color = cum_color + renderer.render(ray)
+        imageT.image.setPixel(col, row, cum_color * (1 / pow(imageT.samples_per_side.float, 2)))
+      else:
+        ray = imageT.fire_ray(col, row)
+        color = renderer.render(ray)
+        imageT.image.setPixel(col, row, color)
+    
