f76ff7383f73ccc7b4290455ecedbc3aabfeaf46
Baratto Matteo <teo.baratto.97@gmail.com>
2022-06-13 21:45:26 +0200

    Merge

--- scripts/test-parallel.sh
+++ scripts/test-parallel.sh
@@ -1,5 +1,5 @@
 for angle in $1; do
-	./trayx render exampleInput.txt 480 360 --clock=$angle --output=$angle.png
+	./trayx render exampleInput.txt 480 360 --clock=$angle --output=result/$angle.png
 done
 
 # parallel -j NUM_OF_CORES ./test-parallel.sh '{}' ::: $(seq 0 359)
